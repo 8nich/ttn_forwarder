97deaef47747e1742e6ba7530bc1701a2e7ee470
8nich <s8nich@gmail.com>
2022-01-24 06:55:43 +0100

    data to datarame to mysql test

--- app.py
+++ app.py
@@ -17,14 +17,41 @@ app = Flask(__name__)
 #@app.route('/up')
 @app.route('/up', methods=['POST'])
 def forward_up():
-    #influx_response = influxdb(request.json['payload_fields'])
-    #success = len(influx_response) == 0
-    #df =
-    #with engine.connect() as con:
-    #    df.to_sql(name=table, con=con, if_exists='replace', index=True)
-    #engine.dispose()
-    #print(f"insert done into: {table}")
     print(json.dumps(request.json, indent=4, sort_keys=True))
+    df = pd.DataFrame([request.json.uplink_message.received_at,
+            request.json.end_device_ids.dev_eui,
+            request.jsond.uplink_message.decoded_payload.analog_in_4,
+            request.json.uplink_message.decoded_payload.luminosity_6,
+            request.json.uplink_message.decoded_payload.temperature_5,
+            request.json.uplink_message.decoded_payload.digital_in_1,
+            request.json.uplink_message.decoded_payload.digital_in_2,
+            request.json.uplink_message.decoded_payload.digital_in_3,
+            request.json.uplink_message.decoded_payload.accelerometer_7.x,
+            request.json.uplink_message.decoded_payload.accelerometer_7.y,
+            request.json.uplink_message.decoded_payload.accelerometer_7.z,
+            request.json.uplink_message.rx_metadata.length,
+            request.json.uplink_message.settings.data_rate.lora.bandwidth,
+            request.json.uplink_message.settings.data_rate.lora.spreading_factor],
+        columns=[   'timestamp1',
+                    'hardware_serial',
+                    'battery',
+                    'luminosity',
+                    'temperature',
+                    'digital_in1',
+                    'digital_in2',
+                    'digital_in3',
+                    'acc_x',
+                    'acc_y',
+                    'acc_z',
+                    'gateways',
+                    'BW',
+                    'SF'])
+    pd['timestamp1'] = pd.to_datetime(df['timestamp1'])
+    print(df)
+    with engine.connect() as con:
+        df.to_sql(name=table, con=con, if_exists='replace', index=True)
+    engine.dispose()
+    print(f"insert done into: {table}")
 
     return jsonify(success=1, response = "ok")
     #return jsonify(success=success,
